de19713dae91d455f9bca9678fe27664473e113f
vyshu1234567 <vyshnavibalu10b@gmail.com>
2023-09-17 16:07:46 +0530

    adding file

--- main.js
+++ main.js
@@ -1,49 +1,91 @@
 var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
+var item = document.getElementById('items');
+var itemList = document.getElementsByClassName('list-group-item');
+var filter = document.getElementById('filter')
 
-// Form submit event
-form.addEventListener('submit', addItem);
-// Delete event
-itemList.addEventListener('click', removeItem);
+filter.addEventListener('keyup', onFilter);
+form.addEventListener('submit' , onsubmit);
+item.addEventListener('click', ondelete);
 
+// adding edit button to all the list item
+for(let i=0;i<itemList.length;i++) {
 
-// Add item
-function addItem(e){
-  e.preventDefault();
+  const editBtn = document.createElement('button');
+  editBtn.className =  `btn btn-sm float-right editBtn`;
+  editBtn.appendChild(document.createTextNode('EDIT'))
 
-  // Get input value
-  var newItem = document.getElementById('item').value;
-
-  // Create new li element
-  var li = document.createElement('li');
-  // Add class
-  li.className = 'list-group-item';
-  // Add text node with input value
-  li.appendChild(document.createTextNode(newItem));
-
-  // Create del button element
-  var deleteBtn = document.createElement('button');
-
-  // Add classes to del button
-  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
-
-  // Append text node
-  deleteBtn.appendChild(document.createTextNode('X'));
-
-  // Append button to li
-  li.appendChild(deleteBtn);
-
-  // Append li to list
-  itemList.appendChild(li);
+  itemList[i].appendChild(editBtn)
 }
 
-// Remove item
-function removeItem(e){
-  if(e.target.classList.contains('delete')){
-    if(confirm('Are You Sure?')){
-      var li = e.target.parentElement;
-      itemList.removeChild(li);
+function onsubmit(e) {
+  e.preventDefault();
+
+  const inputvalue = document.getElementById('item').value;
+  const description = document.getElementById('description').value;
+
+  const li = document.createElement('li');
+
+  li.className = 'list-group-item';
+   
+  const newText = document.createTextNode(inputvalue)
+  const descriptionNode = document.createTextNode(description)
+
+  li.appendChild(newText);
+  li.appendChild(descriptionNode);
+
+  const deleteBtn = document.createElement('button');
+
+  deleteBtn.className = `btn btn-danger btn-sm float-right delete`;
+
+  deleteBtn.appendChild(document.createTextNode('x'))
+
+  li.appendChild(deleteBtn);
+
+  // Adding editButton
+  const editBtn = document.createElement('button');
+  editBtn.className = `btn btn-sm float-right editBtn`;
+  editBtn.appendChild(document.createTextNode('EDit'));
+
+  li.appendChild(editBtn)
+
+  item.appendChild(li)
+  }
+
+function ondelete(e) {
+  if(e.target.classList.contains('delete')) {
+
+    if(confirm('are you sure?')) {
+
+      const li = e.target.parentElement;
+
+      item.removeChild(li);
     }
   }
 }
+
+// filter
+
+
+function onFilter(e){
+  // convert text to lowercase
+  var text = e.target.value.toLowerCase();
+  
+  // getting list
+  var items = item.getElementsByTagName('li');
+
+  // Converting item from HTML collection to array
+  Array.from(items).forEach(function(item) {
+
+    var itemName = item.firstChild.textContent;
+    var description = item.childNodes[1].textContent;
+
+    if(itemName.toLowerCase().indexOf(text) != -1 || description.toLowerCase().indexOf(text) != -1) {
+      
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
+
 
